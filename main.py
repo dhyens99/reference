27fbefc1393bf2a0134d3d4edd15cbeac805d675
Dong-Hyeon <dhyens99@gmail.com>
2026-05-28 01:33:32 +0900

    merge: reslove conflict

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 def main():
     print("Hello from dev!")
     print("conflict branch practice")
+<<<<<<< HEAD
     print("main.ver")
+=======
+    print("conflict ver")
+>>>>>>> feat/conflict
 
 if __name__ == "__main__":
     main()
