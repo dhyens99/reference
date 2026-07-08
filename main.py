07b037cd6abd527963f3475b8dfe603d00f5ada4
Dong-Hyeon <dhyens99@gmail.com>
2026-07-09 01:04:41 +0900

    test

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
 def main():
     print("Hello from dev!")
     print("conflict branch practice")
-<<<<<<< HEAD
     print("ddddddddd")
-=======
-    print("aaaaaaaaaaaa")
->>>>>>> feat/conflict
+
 
 if __name__ == "__main__":
     main()
